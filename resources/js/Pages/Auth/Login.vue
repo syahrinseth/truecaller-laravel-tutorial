e479481512288ec8a22827e11c16e409d16bc925
syahrinseth <syahrinsethservices@gmail.com>
2023-08-10 00:28:32 +0800

    first commit

---
+++ resources/js/Pages/Auth/Login.vue
@@ -0,0 +1,137 @@
+<script setup>
+import Checkbox from '@/Components/Checkbox.vue';
+import GuestLayout from '@/Layouts/GuestLayout.vue';
+import InputError from '@/Components/InputError.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import TextInput from '@/Components/TextInput.vue';
+import { Head, Link, useForm } from '@inertiajs/vue3';
+import { onMounted } from 'vue';
+
+defineProps({
+    canResetPassword: Boolean,
+    status: String,
+});
+
+const form = useForm({
+    email: '',
+    password: '',
+    remember: false,
+});
+
+const submit = () => {
+    form.post(route('login'), {
+        onFinish: () => form.reset('password'),
+    });
+};
+
+const makeid = (length) => {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        counter += 1;
+    }
+    return result;
+}
+
+onMounted(() => {
+    window.location = `truecallersdk://truesdk/web_verify?
+        type=btmsheet
+        requestNonce=${makeid(64)}
+        &partnerKey=${import.meta.env?.VITE_PUSHER_TRUECALLER_APP_KEY}
+        &partnerName=${import.meta.env?.VITE_PUSHER_TRUECALLER_APP_NAME}
+        &lang=en
+        &privacyUrl=""
+        &termsUrl=""
+        &loginPrefix=continue
+        &loginSuffix=login
+        &ctaPrefix=use
+        &ctaColor=%23f75d34
+        &ctaTextColor=%23f75d34
+        &btnShape=round
+        &skipOption=later
+        &ttl=`;
+
+    setTimeout(function() {
+
+        if( document.hasFocus() ){
+            // Truecaller app not present on the device and you redirect the user 
+            // to your alternate verification page
+            alert('Truecaller not defined.');
+        }else{
+            // Truecaller app present on the device and the profile overlay opens
+            // The user clicks on verify & you'll receive the user's access token to fetch the profile on your 
+            // callback URL - post which, you can refresh the session at your frontend and complete the user verification
+            alert('Truecaller present.')
+        }
+
+    }, 600);
+})
+</script>
+
+<template>
+    <GuestLayout>
+        <Head title="Log in" />
+
+        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
+            {{ status }}
+        </div>
+
+        <form @submit.prevent="submit">
+            <div>
+                <InputLabel for="email" value="Email" />
+
+                <TextInput
+                    id="email"
+                    type="email"
+                    class="mt-1 block w-full"
+                    v-model="form.email"
+                    required
+                    autofocus
+                    autocomplete="username"
+                />
+
+                <InputError class="mt-2" :message="form.errors.email" />
+            </div>
+
+            <div class="mt-4">
+                <InputLabel for="password" value="Password" />
+
+                <TextInput
+                    id="password"
+                    type="password"
+                    class="mt-1 block w-full"
+                    v-model="form.password"
+                    required
+                    autocomplete="current-password"
+                />
+
+                <InputError class="mt-2" :message="form.errors.password" />
+            </div>
+
+            <div class="block mt-4">
+                <label class="flex items-center">
+                    <Checkbox name="remember" v-model:checked="form.remember" />
+                    <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">Remember me</span>
+                </label>
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                <Link
+                    v-if="canResetPassword"
+                    :href="route('password.request')"
+                    class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                >
+                    Forgot your password?
+                </Link>
+
+                <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                    Log in
+                </PrimaryButton>
+            </div>
+        </form>
+    </GuestLayout>
+</template>
